4776f69569f24d98cc71778eddd552db8240b646
Asfath Shifan <shifan@parkwaylabs.com>
2026-06-24 18:42:44 +0530

    added meaning full coimment in customer seerviuce

--- customer/src/services/customer-service.test.js
+++ customer/src/services/customer-service.test.js
@@ -1,6 +1,6 @@
 // Which service
 describe('CustomerService', () => {
-  // Which Function
+  // Which Function we are testing
   describe('SignIn', () => {
     // Which scenario we are testing
     test('validate user inputs', () => {});
